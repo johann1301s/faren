deb598c134cfcf525d69798f01b9b73ab9d37a5b
johann1301s <johann1301h@icloud.com>
2020-04-25 10:46:44 +0200

    update page

---
+++ script.js
@@ -0,0 +1,8 @@
+const steinmann = document.querySelector('steinmann');
+const top = document.querySelector('steinmann');
+const right = document.querySelector('steinmann');
+const left = document.querySelector('steinmann');
+
+
+
+
