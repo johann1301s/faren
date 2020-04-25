683e2c336f611fc89dac6bfd1ca80a7f9d0d1951
johann1301s <johann1301h@icloud.com>
2020-04-25 10:49:14 +0200

    update script

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ const right = document.querySelector('steinmann');
 const left = document.querySelector('steinmann');
 
 
+top.addEventListener('click', () => {
+  window.alert('opp');
+})
 
+right.addEventListener('click', () => {
+  window.alert('høyre');
+})
+
+left.addEventListener('click', () => {
+  window.alert('venstre');
+})
 
