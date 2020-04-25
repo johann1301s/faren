50350234c8e9ddb89e1d0013402d50385f525ff4
johann1301s <johann1301h@icloud.com>
2020-04-25 11:09:36 +0200

    wip

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
+const container = document.querySelector('#container');
+
 const steinmann = document.querySelector('#steinmann');
 const topButton = document.querySelector('.top');
 const rightButton = document.querySelector('.right');
 const leftButton = document.querySelector('.left');
 
+const frameHeight = container.offsetHeight;
+const frameHeight = container.offsetWidth;
 
 topButton.addEventListener('click', () => {
-  steinmann.style.top = parseFloat(steinmann.style.top) - 10 + 'px';
+  steinmann.style.top = parseFloat(steinmann.style.top) - 100 + 'px';
 })
 
 rightButton.addEventListener('click', () => {
-  steinmann.style.left = parseFloat(steinmann.style.left) + 10 + 'px';
+  steinmann.style.left = parseFloat(steinmann.style.left) + 100 + 'px';
 })
 
 leftButton.addEventListener('click', () => {
-  steinmann.style.left = parseFloat(steinmann.style.left) - 10 + 'px';
+  steinmann.style.left = parseFloat(steinmann.style.left) - 100 + 'px';
 })
 
+function loop() {
+  
+  
+  
+  requestAnimationFrame(loop);
+}
+
+requestAnimationFrame()
